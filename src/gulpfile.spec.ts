da9bd872c3659e9dc325d03550efe91b975cad55
Pathurs <pathurs123@gmail.com>
2018-11-11 11:39:29 +1100

    test: fix stub

--- src/gulpfile.spec.ts
+++ src/gulpfile.spec.ts
@@ -1,10 +1,9 @@
 import { Expect, Test, TestFixture } from 'alsatian';
-import { GulpFile } from './gulpfile';
 
-@TestFixture('GulpFile Tests')
-export class GulpFileTests {
-    @Test('should be created')
-    public create1() {
-        Expect(GulpFile).toBeDefined();
+@TestFixture('Stub')
+export class Stub {
+    @Test('stub')
+    public stub() {
+        Expect(true).toBe(true);
     }
 }
