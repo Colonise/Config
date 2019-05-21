e805b1c996aa6b627f3da42fa9f6b1fe5bf2a611
Pathurs <pathurs123@gmail.com>
2019-05-21 19:00:39 +1000

    config(tsconfig): replace typeRoots with paths

--- @types/@semantic-release/error/index.d.ts
+++ @types/@semantic-release/error/index.d.ts
@@ -1,11 +1,9 @@
-declare class SemanticReleaseError extends Error {
-    public name: string;
-    public message: string;
-    public code: string;
-    public details: string;
-    public constructor(message: string, code: string, details: string);
-}
-
 declare module '@semantic-release/error' {
-    export = SemanticReleaseError;
+    export default class SemanticReleaseError extends Error {
+        public name: string;
+        public message: string;
+        public code: string;
+        public details: string;
+        public constructor(message: string, code: string, details: string);
+    }
 }
