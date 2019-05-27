171b5dfafb9da6e5d2313022567a9d0edd5ecfb7
Pathurs <pathurs123@gmail.com>
2019-05-27 12:40:39 +1000

    fix(install): add middle-man installation script
    middle-man installation script checks to see if it *should* run the
    installation script

---
+++ install.js
@@ -0,0 +1,7 @@
+const appRootPath = require('app-root-path');
+const rootPackageJson = appRootPath.require('./package.json');
+const ourPackageJson = require('./package.json');
+
+if (rootPackageJson.name !== ourPackageJson.name) {
+    require('./dist/install.js');
+}
