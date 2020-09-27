9f55b8054efbf21c28f23a5f6d04862064e7013b
Patrick Stanford <contact@pathurs.com>
2020-09-27 13:31:48 +1000

    fix(coverage): properly export nyc config

--- nyc.config.js
+++ nyc.config.js
@@ -1,11 +1,1 @@
-module.exports = {
-    all: true,
-    reporter: ['lcov', 'html', 'text'],
-    'report-dir': './coverage',
-    include: [
-        'build/**/*.js'
-    ],
-    exclude: [
-        '**/*.spec.*'
-    ]
-};
+module.exports = require('./build/nyc');
